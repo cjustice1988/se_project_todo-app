db77ebdde6e528fc746d6dfb115b8d3022dfec19
cjustice1988 <cjustice1988@gmail.com>
2026-02-21 15:58:29 -0600

    part 6 sprint 7 started refactoring FormValidator

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import FormValidator from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -85,3 +86,6 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
+newTodoValidator.enableValidation();
